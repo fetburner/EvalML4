29283542a708d55752775ecac11ef99a4ccb1c63
udondaisuki <mythsuma@yahoo.co.jp>
2013-04-19 01:42:30 +0900

    記述量を削減

--- deriv.ml
+++ deriv.ml
@@ -34,95 +34,97 @@ let string_of_eval_desc { env = env; exp = e; value = v} =
  * val to_string_aux : int -> int -> t -> string
  * 指定された幅でインデントしながら導出を文字列で表現する
  *)
-let rec to_string_aux indent depth = function
+let rec to_string_aux indent depth =
+  let to_string_aux_with_indent d = to_string_aux indent (depth + indent) d in
+  let make_indent depth = String.make depth ' ' in function
   | EInt (desc) ->
-      String.make depth ' ' ^ string_of_eval_desc desc ^ "E-Int {}"
+      make_indent depth ^ string_of_eval_desc desc ^ "E-Int {}"
   | EBool (desc) ->
-      String.make depth ' ' ^ string_of_eval_desc desc ^ "E-Bool {}"
+      make_indent depth ^ string_of_eval_desc desc ^ "E-Bool {}"
   | EVar (desc) ->
-      String.make depth ' ' ^ string_of_eval_desc desc ^ "E-Var {}"
+      make_indent depth ^ string_of_eval_desc desc ^ "E-Var {}"
   | EPlus (desc, d1, d2, d3) ->
-      String.make depth ' ' ^ string_of_eval_desc desc ^ "E-Plus {\n" ^
-      to_string_aux indent (depth + indent) d1 ^ ";\n" ^
-      to_string_aux indent (depth + indent) d2 ^ ";\n" ^
-      to_string_aux indent (depth + indent) d3 ^ "\n" ^
-      String.make depth ' ' ^ "}"
+      make_indent depth ^ string_of_eval_desc desc ^ "E-Plus {\n" ^
+      to_string_aux_with_indent d1 ^ ";\n" ^
+      to_string_aux_with_indent d2 ^ ";\n" ^
+      to_string_aux_with_indent d3 ^ "\n" ^
+      make_indent depth ^ "}"
   | EMinus (desc, d1, d2, d3) ->
-      String.make depth ' ' ^ string_of_eval_desc desc ^ "E-Minus {\n" ^
-      to_string_aux indent (depth + indent) d1 ^ ";\n" ^
-      to_string_aux indent (depth + indent) d2 ^ ";\n" ^
-      to_string_aux indent (depth + indent) d3 ^ "\n" ^
-      String.make depth ' ' ^ "}"
+      make_indent depth ^ string_of_eval_desc desc ^ "E-Minus {\n" ^
+      to_string_aux_with_indent d1 ^ ";\n" ^
+      to_string_aux_with_indent d2 ^ ";\n" ^
+      to_string_aux_with_indent d3 ^ "\n" ^
+      make_indent depth ^ "}"
   | ETimes (desc, d1, d2, d3) ->
-      String.make depth ' ' ^ string_of_eval_desc desc ^ "E-Times {\n" ^
-      to_string_aux indent (depth + indent) d1 ^ ";\n" ^
-      to_string_aux indent (depth + indent) d2 ^ ";\n" ^
-      to_string_aux indent (depth + indent) d3 ^ "\n" ^
-      String.make depth ' ' ^ "}"
+      make_indent depth ^ string_of_eval_desc desc ^ "E-Times {\n" ^
+      to_string_aux_with_indent d1 ^ ";\n" ^
+      to_string_aux_with_indent d2 ^ ";\n" ^
+      to_string_aux_with_indent d3 ^ "\n" ^
+      make_indent depth ^ "}"
   | ELt (desc, d1, d2, d3) ->
-      String.make depth ' ' ^ string_of_eval_desc desc ^ "E-Lt {\n" ^
-      to_string_aux indent (depth + indent) d1 ^ ";\n" ^
-      to_string_aux indent (depth + indent) d2 ^ ";\n" ^
-      to_string_aux indent (depth + indent) d3 ^ "\n" ^
-      String.make depth ' ' ^ "}"
+      make_indent depth ^ string_of_eval_desc desc ^ "E-Lt {\n" ^
+      to_string_aux_with_indent d1 ^ ";\n" ^
+      to_string_aux_with_indent d2 ^ ";\n" ^
+      to_string_aux_with_indent d3 ^ "\n" ^
+      make_indent depth ^ "}"
   | EIfT (desc, d1, d2) ->
-      String.make depth ' ' ^ string_of_eval_desc desc ^ "E-IfT {\n" ^
-      to_string_aux indent (depth + indent) d1 ^ ";\n" ^
-      to_string_aux indent (depth + indent) d2 ^ "\n" ^
-      String.make depth ' ' ^ "}"
+      make_indent depth ^ string_of_eval_desc desc ^ "E-IfT {\n" ^
+      to_string_aux_with_indent d1 ^ ";\n" ^
+      to_string_aux_with_indent d2 ^ "\n" ^
+      make_indent depth ^ "}"
   | EIfF (desc, d1, d2) ->
-      String.make depth ' ' ^ string_of_eval_desc desc ^ "E-IfF {\n" ^
-      to_string_aux indent (depth + indent) d1 ^ ";\n" ^
-      to_string_aux indent (depth + indent) d2 ^ "\n" ^
-      String.make depth ' ' ^ "}"
+      make_indent depth ^ string_of_eval_desc desc ^ "E-IfF {\n" ^
+      to_string_aux_with_indent d1 ^ ";\n" ^
+      to_string_aux_with_indent d2 ^ "\n" ^
+      make_indent depth ^ "}"
   | ELet (desc, d1, d2) ->
-      String.make depth ' ' ^ string_of_eval_desc desc ^ "E-Let {\n" ^
-      to_string_aux indent (depth + indent) d1 ^ ";\n" ^
-      to_string_aux indent (depth + indent) d2 ^ "\n" ^
-      String.make depth ' ' ^ "}"
+      make_indent depth ^ string_of_eval_desc desc ^ "E-Let {\n" ^
+      to_string_aux_with_indent d1 ^ ";\n" ^
+      to_string_aux_with_indent d2 ^ "\n" ^
+      make_indent depth ^ "}"
   | EFun (desc) ->
-      String.make depth ' ' ^ string_of_eval_desc desc ^ "E-Fun {}"
+      make_indent depth ^ string_of_eval_desc desc ^ "E-Fun {}"
   | EApp (desc, d1, d2, d3) ->
-      String.make depth ' ' ^ string_of_eval_desc desc ^ "E-App {\n" ^
-      to_string_aux indent (depth + indent) d1 ^ ";\n" ^
-      to_string_aux indent (depth + indent) d2 ^ ";\n" ^
-      to_string_aux indent (depth + indent) d3 ^ "\n" ^
-      String.make depth ' ' ^ "}"
+      make_indent depth ^ string_of_eval_desc desc ^ "E-App {\n" ^
+      to_string_aux_with_indent d1 ^ ";\n" ^
+      to_string_aux_with_indent d2 ^ ";\n" ^
+      to_string_aux_with_indent d3 ^ "\n" ^
+      make_indent depth ^ "}"
   | ELetRec (desc, d) ->
-      String.make depth ' ' ^ string_of_eval_desc desc ^ "E-LetRec {\n" ^
-      to_string_aux indent (depth + indent) d ^ "\n" ^
-      String.make depth ' ' ^ "}"
+      make_indent depth ^ string_of_eval_desc desc ^ "E-LetRec {\n" ^
+      to_string_aux_with_indent d ^ "\n" ^
+      make_indent depth ^ "}"
   | EAppRec (desc, d1, d2, d3) ->
-      String.make depth ' ' ^ string_of_eval_desc desc ^ "E-AppRec {\n" ^
-      to_string_aux indent (depth + indent) d1 ^ ";\n" ^
-      to_string_aux indent (depth + indent) d2 ^ ";\n" ^
-      to_string_aux indent (depth + indent) d3 ^ "\n" ^
-      String.make depth ' ' ^ "}"
+      make_indent depth ^ string_of_eval_desc desc ^ "E-AppRec {\n" ^
+      to_string_aux_with_indent d1 ^ ";\n" ^
+      to_string_aux_with_indent d2 ^ ";\n" ^
+      to_string_aux_with_indent d3 ^ "\n" ^
+      make_indent depth ^ "}"
   | ENil (desc) -> 
-      String.make depth ' ' ^ string_of_eval_desc desc ^ "E-Nil {}"
+      make_indent depth ^ string_of_eval_desc desc ^ "E-Nil {}"
   | ECons (desc, d1, d2) ->
-      String.make depth ' ' ^ string_of_eval_desc desc ^ "E-Cons {\n" ^
-      to_string_aux indent (depth + indent) d1 ^ ";\n" ^
-      to_string_aux indent (depth + indent) d2 ^ "\n" ^
-      String.make depth ' ' ^ "}"
+      make_indent depth ^ string_of_eval_desc desc ^ "E-Cons {\n" ^
+      to_string_aux_with_indent d1 ^ ";\n" ^
+      to_string_aux_with_indent d2 ^ "\n" ^
+      make_indent depth ^ "}"
   | EMatchNil (desc, d1, d2) ->
-      String.make depth ' ' ^ string_of_eval_desc desc ^ "E-MatchNil {\n" ^
-      to_string_aux indent (depth + indent) d1 ^ ";\n" ^
-      to_string_aux indent (depth + indent) d2 ^ "\n" ^
-      String.make depth ' ' ^ "}"
+      make_indent depth ^ string_of_eval_desc desc ^ "E-MatchNil {\n" ^
+      to_string_aux_with_indent d1 ^ ";\n" ^
+      to_string_aux_with_indent d2 ^ "\n" ^
+      make_indent depth ^ "}"
   | EMatchCons (desc, d1, d2) ->
-      String.make depth ' ' ^ string_of_eval_desc desc ^ "E-MatchCons {\n" ^
-      to_string_aux indent (depth + indent) d1 ^ ";\n" ^
-      to_string_aux indent (depth + indent) d2 ^ "\n" ^
-      String.make depth ' ' ^ "}"
+      make_indent depth ^ string_of_eval_desc desc ^ "E-MatchCons {\n" ^
+      to_string_aux_with_indent d1 ^ ";\n" ^
+      to_string_aux_with_indent d2 ^ "\n" ^
+      make_indent depth ^ "}"
   | BPlus { lsrc = lsrc; rsrc = rsrc; dst = dst } ->
-      String.make depth ' ' ^ Value.to_string lsrc ^ " plus " ^ Value.to_string rsrc ^ " is " ^ Value.to_string dst ^ " by B-Plus {}"
+      make_indent depth ^ Value.to_string lsrc ^ " plus " ^ Value.to_string rsrc ^ " is " ^ Value.to_string dst ^ " by B-Plus {}"
   | BMinus { lsrc = lsrc; rsrc = rsrc; dst = dst } ->
-      String.make depth ' ' ^ Value.to_string lsrc ^ " minus " ^ Value.to_string rsrc ^ " is " ^ Value.to_string dst ^ " by B-Minus {}"
+      make_indent depth ^ Value.to_string lsrc ^ " minus " ^ Value.to_string rsrc ^ " is " ^ Value.to_string dst ^ " by B-Minus {}"
   | BTimes { lsrc = lsrc; rsrc = rsrc; dst = dst } ->
-      String.make depth ' ' ^ Value.to_string lsrc ^ " times " ^ Value.to_string rsrc ^ " is " ^ Value.to_string dst ^ " by B-Times {}"
+      make_indent depth ^ Value.to_string lsrc ^ " times " ^ Value.to_string rsrc ^ " is " ^ Value.to_string dst ^ " by B-Times {}"
   | BLt { lsrc = lsrc; rsrc = rsrc; dst = dst } ->
-      String.make depth ' ' ^ Value.to_string lsrc ^ " less than " ^ Value.to_string rsrc ^ " is " ^ Value.to_string dst ^ " by B-Lt {}"
+      make_indent depth ^ Value.to_string lsrc ^ " less than " ^ Value.to_string rsrc ^ " is " ^ Value.to_string dst ^ " by B-Lt {}"
 
 let to_string = to_string_aux 2 0
 
@@ -130,16 +132,18 @@ let to_string = to_string_aux 2 0
  * val eval_and_deriv : Value.env -> Exp.t -> t * Value.t
  * 与えられた式を評価しつつ導出を残す
  *)
-let rec eval_and_deriv env = function
+let rec eval_and_deriv env exp =
+  let make_eval_desc v = { env = env; exp = exp; value = v } in
+  match exp with
   | Exp.Int (i) -> 
       let v = Value.Int (i) in
-      (EInt { env = env; exp = Exp.Int (i); value = v }, v)
+      (EInt (make_eval_desc v), v)
   | Exp.Bool (b) ->
       let v = Value.Bool (b) in
-      (EBool { env = env; exp = Exp.Bool (b); value = v }, v)
+      (EBool (make_eval_desc v), v)
   | Exp.Var (x) ->
       let v = List.assoc x env in
-      (EVar { env = env; exp = Exp.Var (x); value = v }, v)
+      (EVar (make_eval_desc v), v)
   | Exp.BinOp (e1, op, e2) ->
       let (d1, v1) = eval_and_deriv env e1 in
       let (d2, v2) = eval_and_deriv env e2 in
@@ -147,67 +151,67 @@ let rec eval_and_deriv env = function
       | Value.Int (i1), Prim.Plus, Value.Int (i2) ->
           let v = Value.Int (i1 + i2) in
           let d3 = BPlus { lsrc = v1; rsrc = v2; dst = v} in
-          (EPlus ({ env = env; exp = Exp.BinOp (e1, op, e2); value = v}, d1, d2, d3), v)
+          (EPlus (make_eval_desc v, d1, d2, d3), v)
       | Value.Int (i1), Prim.Minus, Value.Int (i2) ->
           let v = Value.Int (i1 - i2) in
           let d3 = BMinus { lsrc = v1; rsrc = v2; dst = v} in
-          (EMinus({ env = env; exp = Exp.BinOp (e1, op, e2); value = v}, d1, d2, d3), v)
+          (EMinus (make_eval_desc v, d1, d2, d3), v)
       | Value.Int (i1), Prim.Times, Value.Int (i2) ->
           let v = Value.Int (i1 * i2) in
           let d3 = BTimes { lsrc = v1; rsrc = v2; dst = v} in
-          (ETimes ({ env = env; exp = Exp.BinOp (e1, op, e2); value = v}, d1, d2, d3), v)
+          (ETimes (make_eval_desc v, d1, d2, d3), v)
       | Value.Int (i1), Prim.Lt, Value.Int (i2) ->
           let v = Value.Bool (i1 < i2) in
           let d3 = BLt { lsrc = v1; rsrc = v2; dst = v} in
-          (ELt ({ env = env; exp = Exp.BinOp (e1, op, e2); value = v}, d1, d2, d3), v)
+          (ELt (make_eval_desc v, d1, d2, d3), v)
       end
   | Exp.If (e1, e2, e3) ->
       let (d1, v1) = eval_and_deriv env e1 in
       begin match v1 with
       | Value.Bool (true) ->
           let (d2, v) = eval_and_deriv env e2 in
-          (EIfT ({ env = env; exp = Exp.If (e1, e2, e3); value = v}, d1, d2), v)
+          (EIfT (make_eval_desc v, d1, d2), v)
       | Value.Bool (false) ->
           let (d3, v) = eval_and_deriv env e3 in
-          (EIfF ({ env = env; exp = Exp.If (e1, e2, e3); value = v}, d1, d3), v)
+          (EIfF (make_eval_desc v, d1, d3), v)
       end
   | Exp.Let (x, e1, e2) ->
       let (d1, v1) = eval_and_deriv env e1 in
       let (d2, v) = eval_and_deriv ((x, v1) :: env) e2 in
-      (ELet ({ env = env; exp = Exp.Let (x, e1, e2); value = v}, d1, d2), v)
+      (ELet (make_eval_desc v, d1, d2), v)
   | Exp.Fun (x, e) ->
       let v = Value.Fun (env, x, e) in
-      (EFun { env = env; exp = Exp.Fun (x, e); value = v}, v)
+      (EFun (make_eval_desc v), v)
   | Exp.App (e1, e2) ->
       let (d1, v1) = eval_and_deriv env e1 in
       let (d2, v2) = eval_and_deriv env e2 in
       begin match v1 with
       | Value.Fun (env2, x, e0) ->
           let (d3, v) = eval_and_deriv ((x, v2) :: env2) e0 in
-          (EApp ({ env = env; exp = Exp.App (e1, e2); value = v }, d1, d2, d3), v)
+          (EApp (make_eval_desc v, d1, d2, d3), v)
       | Value.Rec (env2, x, y, e0) as v1 ->
           let (d3, v) = eval_and_deriv ((y, v2) :: (x, v1) :: env2) e0 in
-          (EAppRec ({ env = env; exp = Exp.App (e1, e2); value = v }, d1, d2, d3), v)
+          (EAppRec (make_eval_desc v, d1, d2, d3), v)
       end
   | Exp.LetRec (x, y, e1, e2) ->
       let (d1, v) = eval_and_deriv ((x, Value.Rec (env, x, y, e1)) :: env) e2 in
-      (ELetRec ({ env = env; exp = Exp.LetRec (x, y, e1, e2); value = v }, d1), v)
+      (ELetRec (make_eval_desc v, d1), v)
   | Exp.Nil ->
-      (ENil { env = env; exp = Exp.Nil; value = Value.Nil }, Value.Nil)
+      (ENil (make_eval_desc Value.Nil ), Value.Nil)
   | Exp.Cons (e1, e2) ->
       let (d1, v1) = eval_and_deriv env e1 in
       let (d2, v2) = eval_and_deriv env e2 in
       let v = Value.Cons (v1, v2) in
-      (ECons ({ env = env; exp = Exp.Cons (e1, e2); value = v}, d1, d2), v)
+      (ECons (make_eval_desc v, d1, d2), v)
   | Exp.Match (e1, e2, x, y, e3) ->
       let (d1, v1) = eval_and_deriv env e1 in
       begin match v1 with
       | Value.Nil ->
           let (d2, v) = eval_and_deriv env e2 in
-          (EMatchNil ({ env = env; exp = Exp.Match (e1, e2, x, y, e3); value = v}, d1, d2), v)
+          (EMatchNil (make_eval_desc v, d1, d2), v)
       | Value.Cons (v1, v2) ->
           let (d3, v) = eval_and_deriv ((y, v2) :: (x, v1) :: env) e3 in
-          (EMatchCons ({ env = env; exp = Exp.Match (e1, e2, x, y, e3); value = v}, d1, d3), v)
+          (EMatchCons (make_eval_desc v, d1, d3), v)
       end
 
 (* F#とかでも見る関数合成的なやつ *)
