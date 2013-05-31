b1d868b8c68fe0fcd73cd121e8d811e51d9f0848
fetburner <mythsuma@yahoo.co.jp>
2013-06-01 03:25:12 +0900

    プリティプリンタを改良した

--- exp.ml
+++ exp.ml
@@ -12,25 +12,47 @@ type t =
   | Cons of t * t
   | Match of t * t * string * string * t
 
-let rec to_string = function
+let priority = function
+  | Match _ | Let _ | Fun _ | LetRec _ -> 0
+  | If _ -> 10
+  | Cons _ -> 20
+  | BinOp (_, Prim.Lt, _) -> 30
+  | BinOp (_, Prim.Plus, _) | BinOp (_, Prim.Minus, _) -> 40
+  | BinOp (_, Prim.Times, _) -> 50
+  | App _ -> 60
+  | Int _ | Bool _ | Var _ | Nil -> 70
+
+let rec to_string_aux prio exp = 
+  (if prio > priority exp then fun str -> "(" ^ str ^ ")"
+  else fun x -> x)
+  (let to_string_assoc = to_string_aux (priority exp) in
+  let to_string_nonassoc = to_string_aux (priority exp + 1) in
+  let ( ^. ) x y = x ^ " " ^ y in
+  match exp with
   | Int (i) -> string_of_int i
   | Bool (b) -> string_of_bool b
   | Var (x) -> x
   | BinOp (e1, op, e2) ->
-      "(" ^ to_string e1 ^ ") " ^ Prim.to_string op ^ " (" ^ to_string e2 ^ ")"
+      to_string_assoc e1 ^. Prim.to_string op ^. to_string_nonassoc e2
   | If (e1, e2, e3) ->
-      "if " ^ to_string e1 ^ " then " ^ to_string e2 ^ " else " ^ to_string e3
+      "if" ^. to_string_assoc e1 ^. "then" ^. to_string_assoc e2 ^.
+      "else" ^. to_string_assoc e3
   | Let (x, e1, e2) ->
-      "let " ^ x ^ " = " ^ to_string e1 ^ " in " ^ to_string e2
+      "let" ^. x ^. "=" ^. to_string_nonassoc e1 ^. "in" ^.
+      to_string_assoc e2
   | Fun (x, e) ->
-      "fun " ^ x ^ " -> " ^ to_string e
+      "fun" ^. x ^. "->" ^. to_string_assoc e
   | App (e1, e2) ->
-      "(" ^ to_string e1 ^ ") (" ^ to_string e2 ^ ")"
+      to_string_assoc e1 ^. to_string_nonassoc e2
   | LetRec (x, y, e1, e2) ->
-      "let rec " ^ x ^ " = fun " ^ y ^ " -> " ^ to_string e1 ^ " in " ^ to_string e2
-  | Nil ->
-      "[]"
+      "let rec" ^. x ^. "= fun" ^. y ^. "->" ^.
+      to_string_assoc e1 ^. "in" ^. to_string_assoc e2
+  | Nil -> "[]"
   | Cons (e1, e2) ->
-      "(" ^ to_string e1 ^ ") :: (" ^ to_string e2 ^ ")"
+      to_string_nonassoc e1 ^. "::" ^. to_string_assoc e2
   | Match (e1, e2, x, y, e3) ->
-      "match " ^ to_string e1 ^ " with [] -> " ^ to_string e2 ^ " | " ^ x ^ " :: " ^ y ^ " -> " ^ to_string e3
+      "match" ^. to_string_assoc e1 ^. "with" ^.
+      "[] ->" ^. to_string_assoc e2 ^.
+      "|" ^. x ^. "::" ^. y ^. "->" ^. to_string_assoc e3)
+
+let to_string = to_string_aux 0
